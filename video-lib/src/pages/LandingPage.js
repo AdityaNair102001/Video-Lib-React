e9b28f58292f00ff1e5d9b71db6617e2a71faf75
AdityaNair102001 <adityanair102001@gmail.com>
2021-09-26 01:02:18 +0530

    Landing Page made

--- video-lib/src/pages/LandingPage.js
+++ video-lib/src/pages/LandingPage.js
@@ -1,19 +1,21 @@
 import "../styles.css";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../AuthProvider";
-import { useContext, useState } from "react";
-import logo from "./eyelogo.png";
-import loading from "../1494.gif";
+import { useContext, useRef, useState } from "react";
+import logo from "../netruh.gif";
+import loading from "../loginLoader.gif";
 
 export function LandingPage() {
   const [field, setField] = useState({});
+
+  const logoRef = useRef("");
 
   const {
     loginWithCredentials,
 
     showLoader,
 
-    loginError
+    loginError,
   } = useContext(AuthContext);
 
   function onChangeHandler(event, { type }) {
@@ -29,72 +31,135 @@ export function LandingPage() {
   }
 
   return (
-    <div className="wrapper-landingpage">
-      {/* <img src="https://media.istockphoto.com/vectors/phoenix-logo-vector-id1086955338?k=6&m=1086955338&s=612x612&w=0&h=hE_76b0Y11r9Hz2HH3QCK2TGq7TO-01M6Kr3EMDjdds=" /> */}
-      <img style={{ margin: "2rem" }} src={logo} width="100%" height="30%" />
+    <div
+      style={{ margin: "auto", backgroundColor: "black", minHeight: "100%" }}
+    >
+      <div style={{}} className="wrapper-landingpage">
+        {/* <img src="https://media.istockphoto.com/vectors/phoenix-logo-vector-id1086955338?k=6&m=1086955338&s=612x612&w=0&h=hE_76b0Y11r9Hz2HH3QCK2TGq7TO-01M6Kr3EMDjdds=" /> */}
 
-      <Link to="/home">
-        <button
-          class="secondary-button-blue"
-          style={{ backgroundColor: "white", color: "black" }}
-        >
-          Browse!
-        </button>
-      </Link>
+        {/* <Logo></Logo> */}
 
-      <h2>Login</h2>
+        <img
+          onMouseEnter={() => {
+            // logoRef.current.style.boxShadow =
+            //   "rgb(119 14 240 / 95%) 0px 0px 20px 5px";
+            logoRef.current.style.boxShadow =
+              "rgb(255 255 255 / 95%) 0px 0px 16px 6px";
+            logoRef.current.style.border = "none";
+          }}
+          onMouseLeave={() => {
+            // logoRef.current.style.boxShadow =
+            //   "rgb(255 255 255 / 95%) 0px 0px 16px 6px";
+            logoRef.current.style.boxShadow = "0 0 0 0";
+            logoRef.current.style.border = "1px white solid";
+          }}
+          ref={logoRef}
+          alt="logo here"
+          className="landing-logo"
+          src={logo}
+          width="400rem"
+          // height="400rem"
+        />
 
-      <div class="inputtext-holder" style={{ margin: "1rem" }}>
-        <input
-          class="input-standard"
-          style={{ width: "20rem" }}
-          type="text"
-          placeholder="Username"
-          onChange={(event) => onChangeHandler(event, { type: "username" })}
-        ></input>
-      </div>
-      <div class="inputtext-holder" style={{ margin: "1rem" }}>
-        <input
-          class="input-standard"
-          style={{ width: "20rem" }}
-          type="password"
-          placeholder="Password"
-          onChange={(event) => onChangeHandler(event, { type: "password" })}
-        ></input>
-      </div>
-
-      <button
-        class="primary-button-blue"
-        style={{ background: "black", color: "white" }}
-        onClick={LoginHandler}
-      >
-        Login
-      </button>
-
-      <div style={{ margin: "1rem" }}>
-        {showLoader ? <img alt="Signing in.." src={loading} /> : ""}
-      </div>
-
-      {loginError ? (
-        <div style={{}} class="alert">
-          <span class="alertText">{loginError}</span>
-        </div>
-      ) : (
-        ""
-      )}
-
-      <h3>
-        Don't have an account?{" "}
-        <em>
-          <Link
-            style={{ color: "black", textDecoration: "none" }}
-            to="/signup"
-            replace
-          >
-            Sign up here!
+        <div>
+          <Link to="/home">
+            <button
+              class="secondary-button-blue"
+              style={{
+                backgroundColor: "white",
+                color: "black",
+                margin: "auto",
+                marginTop: "2rem",
+                boxShadow: "rgb(252 252 252) 0px 0px 11px 0px",
+              }}
+            >
+              Browse!
+            </button>
           </Link>
-        </em>
-      </h3>
+        </div>
+
+        <h2
+          style={{
+            color: "white",
+            fontFamily: "sans-serif",
+            marginTop: "2.5rem",
+          }}
+        >
+          Login
+        </h2>
+
+        <div class="inputtext-holder" style={{ margin: "1rem" }}>
+          <input
+            class="input-standard"
+            style={{ width: "20rem", backgroundColor: "black", color: "white" }}
+            type="text"
+            placeholder="Username"
+            onChange={(event) => onChangeHandler(event, { type: "username" })}
+          ></input>
+        </div>
+        <div class="inputtext-holder" style={{ margin: "1rem" }}>
+          <input
+            class="input-standard"
+            style={{ width: "20rem", backgroundColor: "black", color: "white" }}
+            type="password"
+            placeholder="Password"
+            onChange={(event) => onChangeHandler(event, { type: "password" })}
+          ></input>
+        </div>
+        {/* 
+        <div
+          style={{
+            height: "42px",
+            backgroundColor: "white",
+            background:
+              "linear-gradient(to top, rgb(249, 249, 251) 0%, rgb(51, 51, 51) 100%)",
+            width: "10rem",
+            margin: "auto",
+            clipPath:
+              "polygon(25px 0%, calc(100% - 25px) 0%, 100% 100%, 0% 100%)",
+          }}
+        ></div>
+        <button
+          class="primary-button-blue"
+          style={{ background: "white", color: "black", border: "none" }}
+          onClick={LoginHandler}
+        >
+          Login
+        </button> */}
+
+        <div style={{ marginTop: "1rem" }}>
+          <button class="pulse" onClick={() => LoginHandler()}>
+            Login!
+          </button>
+        </div>
+
+        <div style={{ margin: "1rem" }}>
+          {showLoader ? <img alt="Signing in.." src={loading} /> : ""}
+        </div>
+
+        {loginError ? (
+          <div style={{}} class="alert">
+            <span class="alertText">{loginError}</span>
+          </div>
+        ) : (
+          ""
+        )}
+
+        <h3
+          style={{ marginBlock: "0rem", paddingBottom: "1rem", color: "white" }}
+        >
+          Don't have an account?{" "}
+          <em>
+            <Link
+              style={{ color: "white", textDecoration: "none" }}
+              to="/signup"
+              replace
+            >
+              Sign up here!
+            </Link>
+          </em>
+        </h3>
+      </div>
     </div>
   );
 }
